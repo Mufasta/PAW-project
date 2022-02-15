18a4577c80144b4c1632bae2f6ab8584b8c3d104
Cosder <mannan.marium@gmail.com>
2022-02-15 15:37:48 -0600

    Added Home menu item

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import * as ReactBootStrap from "react-bootstrap";
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home />
     </div>
   );
 }
