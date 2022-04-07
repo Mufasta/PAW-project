910969cad0b07087312a215d27bc13db82477b77
1edwin1 <edwin.david.silva.frank@gmail.com>
2022-04-06 20:32:20 -0500

    CatAPI and DogAPI displayed on specific Page

--- paw/src/Components/IndividualCatPage.js
+++ paw/src/Components/IndividualCatPage.js
@@ -1,11 +1,12 @@
 // import React from 'react'
 import { useLocation } from 'react-router-dom'
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 
    
 
 const IndividualCatPage = () => {
+    const [breeds, setBreeds] = useState([])
     const location = useLocation();
 
     useEffect(() =>{
@@ -18,14 +19,25 @@ const IndividualCatPage = () => {
                 'x-api-key': `${process.env.REACT_APP_CAT_API_KEY}`
             }
         }).then(resp => resp.json()) //save the api responses in json format
-        .then(resp =>console.log(resp)) //set the breeds based on the response
+        .then(resp =>setBreeds(resp)) //set the breeds based on the response
     },[])
     
     
     return (
         <>
-            <div>IndividualCatPage</div>
-            
+            {breeds.map(breed => //Sets up the search functionality - links to specific breeds
+                <div className='breeds' key = {breed.id}> 
+                    <div className = 'dataItem'> 
+                        {/*Displays the breed name */}
+                        <h2>{breed.name}</h2>
+                        <h3>Bred for: {breed.bred_for}</h3>    
+                        <h3>Breed group: {breed.breed_group}</h3>  
+                        <h2>{breed.description}</h2>
+                        <h3>Life span: {breed.life_span}</h3>
+                        <h2>Temperament: {breed.temperament}</h2>
+                    </div>
+                </div> 
+            )}
         </>
     
     )
