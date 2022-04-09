7e453c2f9db28573e7d0af92b5995a231295b9ef
1edwin1 <edwin.david.silva.frank@gmail.com>
2022-04-08 22:46:38 -0500

    Image Implemented on Individual Page

--- paw/src/Components/IndividualCatPage.js
+++ paw/src/Components/IndividualCatPage.js
@@ -1,6 +1,8 @@
 // import React from 'react'
 import { useLocation } from 'react-router-dom'
 import React, {useEffect, useState} from "react";
+import CatImage from './CatImage';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 
    
@@ -8,18 +10,24 @@ import React, {useEffect, useState} from "react";
 const IndividualCatPage = () => {
     const [breeds, setBreeds] = useState([])
     const location = useLocation();
+    console.log(location)
+
+    
 
     useEffect(() =>{
         const id = location.pathname.split("/")[2]
         const url = `https://api.thecatapi.com/v1/breeds/search?q=${id}`
-        console.log(url)
         fetch(url, {
             headers: { 
                 //pass in api key to fetch data
                 'x-api-key': `${process.env.REACT_APP_CAT_API_KEY}`
             }
         }).then(resp => resp.json()) //save the api responses in json format
-        .then(resp =>setBreeds(resp)) //set the breeds based on the response
+        .then(resp =>{
+            console.log(resp)
+            setBreeds(resp)
+        }) //set the breeds based on the response
+
     },[])
     
     
@@ -35,8 +43,11 @@ const IndividualCatPage = () => {
                         <h2>{breed.description}</h2>
                         <h3>Life span: {breed.life_span}</h3>
                         <h2>Temperament: {breed.temperament}</h2>
+                        <h2>Image: {breed.reference_image_id}</h2>
+                        <CatImage imgId={breed.reference_image_id}/>
                     </div>
                 </div> 
+                
             )}
         </>
     
